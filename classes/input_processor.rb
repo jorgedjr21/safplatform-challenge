800db488b0898375d8dd5a6a8f0a2f82d8bbf83e
Jorge Junior <jorgedjr21@gmail.com>
2020-10-26 16:15:50 -0300

    input processor spec

---
+++ classes/input_processor.rb
@@ -0,0 +1,49 @@
+require_relative 'product'
+require_relative 'cart'
+
+class InputProcessor
+
+  def initialize(inputs:)
+    @inputs = inputs
+  end
+
+  def to_cart
+    cart = Cart.new
+
+    products_from_input.each do |product|
+      cart.add_product(quantity: product[:quantity], product: product[:product])
+    end
+
+    cart
+  end
+
+  private
+
+  def products_from_input
+    products = []
+    @inputs.each do |product|
+      price    = extract_product_price(product)
+      quantity = extract_product_quantity(product)
+      imported = product.downcase.include?('imported')
+
+      products << {
+        product: Product.new(name: product, price: price, imported: imported),
+        quantity: quantity
+      }
+    end
+
+    products
+  end
+
+  def extract_product_price(input)
+    price = input.scan(/[0-9]+.[0-9]+/).last.to_f
+    input.gsub!(/ at +[0-9]+.[0-9]+/, '')
+    price
+  end
+
+  def extract_product_quantity(input)
+    quantity = input.scan(/[0-9]/).last.to_i
+    input.gsub!(/[0-9]+./, '')
+    quantity
+  end
+end
---
+++ spec/classes/input_processor_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../../classes/input_processor'
+
+RSpec.describe InputProcessor do
+  context 'when defining the class' do
+    it 'must raise exception if file_name is not provided' do
+      expect { InputProcessor.new }.to raise_error(ArgumentError)
+    end
+  end
+
+  context '#to_cart' do
+    let(:inputs) { ['2 book at 12.49', '1 music CD at 14.99', '1 chocolate bar at 0.85'] }
+    subject { InputProcessor.new(inputs: inputs).to_cart }
+
+    it 'must process the input to a cart' do
+      expect(subject.products.count).to eq(3)
+    end
+
+    it 'must have the products names' do
+      names = subject.products.map { |product| product[:product].name }
+
+      expect(names).to eq(['book', 'music CD', 'chocolate bar'])
+    end
+
+    it 'must have the products prices' do
+      prices = subject.products.map { |product| product[:product].price }
+
+      expect(prices).to eq([12.49, 14.99, 0.85])
+    end
+
+    it 'must have the products quantity' do
+      quantity = subject.products.map { |product| product[:quantity] }
+
+      expect(quantity).to eq([2, 1, 1])
+    end
+  end
+end
